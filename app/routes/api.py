70105da8368c29030a8f93c1dc20517208514195
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-11-06 14:19:57 -0500

    sign up/login/logout routes all working properly

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
 
@@ -7,6 +7,53 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 @bp.route('/users', methods=['POST'])
 def signup():
   data = request.get_json()
-  print(data)
+  db = get_db()
 
-  return ''
+  try:
+    #attempt to create new user
+    newUser = User(
+    username = data['username'],
+    email = data['email'],
+    password = data['password']
+  )
+
+    #save in database
+    db.add(newUser)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    #insert failed, so send error to front end
+    db.rollback()
+    return jsonify(message = 'Signup failed'), 500
+
+  session.clear()
+  session['user_id'] = newUser.id
+  session['loggedIn'] = True
+  
+  return jsonify(id = newUser.id)
+
+@bp.route('/users/logout', methods=['POST'])
+def logout():
+  #remove session variables
+  session.clear()
+  return '', 204
+
+@bp.route('/users/login', methods=['POST'])
+def login():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    user = db.query(User).filter(User.email == data['email']).one()
+  except:
+    print(sys.exc_info()[0])
+
+  if user.verify_password(data['password']) == False:
+    return jsonify(message = 'Incorrect credentials'), 400
+
+  session.clear()
+  session['user_id'] = user.id
+  session['loggedIn'] = True
+
+  return jsonify(id = user.id)
